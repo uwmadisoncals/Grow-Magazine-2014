efb93f132dc14ce272cf844e11a205c1c10c0970
Al Nemec <al.nemec@gmail.com>
2014-11-03 13:00:36 -0600

    updated home page call for first feature box

--- home.php
+++ home.php
@@ -171,7 +171,87 @@ while (have_posts()) : the_post();?>
 				<div class="row clearfix">
 
 
-<?php query_posts(array('category__and' => array(17, $current_issue), "showposts" => '2', 'orderby'=>'date', 'order'=> 'desc')); ?>
+<?php query_posts(array('category__and' => array(26, $current_issue), "showposts" => '1', 'orderby'=>'date', 'order'=> 'desc')); ?>
+
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post();  ?>
+
+					<div class="span-50 box dropin">
+
+							<h2>Feature</h2>
+
+							
+	
+
+
+
+
+
+  <?php	if ( has_post_thumbnail() ) {
+
+		    				//the_post_thumbnail();
+		    				echo get_the_post_thumbnail($page->ID, 'medium');
+
+		    				} else {
+							//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
+							 //echo '<img src="';
+							 //echo catch_that_news_image();
+							// echo '" alt="" />';
+
+		    					 
+							//get article image from flickr
+							grow_get_article_image($size='medium');
+		
+
+						} ?>
+			<div class="boxContent">
+											<h3 class="spotlight_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h3>
+											<p><?php the_excerpt(); ?></p>
+                                             </div>
+                            <div class="topShade"></div>
+							<div class="bottomShade"></div>
+
+
+
+
+
+
+
+
+							<a href="http://news.cals.wisc.edu" class="moreButton">More Featured</a>
+
+
+						<div class="windows8">
+							<div class="wBall" id="wBall_1">
+							<div class="wInnerBall">
+							</div>
+							</div>
+							<div class="wBall" id="wBall_2">
+							<div class="wInnerBall">
+							</div>
+							</div>
+							<div class="wBall" id="wBall_3">
+							<div class="wInnerBall">
+							</div>
+							</div>
+							<div class="wBall" id="wBall_4">
+							<div class="wInnerBall">
+							</div>
+							</div>
+							<div class="wBall" id="wBall_5">
+							<div class="wInnerBall">
+							</div>
+							</div>
+						</div>
+
+						<div class="shade"></div>
+
+					</div>
+
+	  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php query_posts(array('category__and' => array(17, $current_issue), "showposts" => '1', 'offset' => '1', 'orderby'=>'date', 'order'=> 'desc')); ?>
 
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post();  ?>
