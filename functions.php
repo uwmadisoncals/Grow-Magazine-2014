fb8255fbcba3ec91004700b22c0cc9cddfb8810d
Daniel Dropik <ddropik@wisc.edu>
2015-06-11 11:04:01 -0500

    in_the_field template css, and markup
    Also, Added Read More function to functions.php for better readability on default link.

--- functions.php
+++ functions.php
@@ -1419,3 +1419,9 @@ function cals_get_last_tweet(){
 
 
 }
+//Readmore
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( '... Read More', 'your-text-domain' ) . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
--- in_the_field.php
+++ in_the_field.php
@@ -5,7 +5,10 @@
 
 get_header(); ?>
 
-<?php //$current_issue = get_option('current_issue'); ?>
+<?php $current_issue = get_option('current_issue');
+
+//logit( $current_issue, '$current_issue:' ); 
+ ?>
 
 
 	<div id="main">
@@ -14,42 +17,34 @@ get_header(); ?>
 		
 			<div id="content" role="main">
 
-			<?php query_posts(array('cat' => '470', 'category__and' => array($current_issue), "showposts" => '1', 'orderby'=>'title', 'order'=> 'desc')); ?>
+			<?php /* query_posts(array('cat' => '470', 'category__and' => array($current_issue), "showposts" => '1', 'orderby'=>'title', 'order'=> 'desc')); */ ?>
 
-			<?php if (have_posts()) : ?>
-			 	<?php while (have_posts()) : the_post();  ?>
+			<?php 
+			$args = array(
+				'posts_per_page'=>'-1',
+				'orderby'=>'title',
+				'order'=>'desc',
+				//'cat'=>'470',
+				//'category__and'=>'array($current_issue)'
+				'category__and' => array(470,$current_issue)
+				);
+
+			$inTheField_query = new WP_Query($args);
+
+			 ?>
+
+	<header class="entry-header inTheField">
+		<h1 class="entry-title"><?php single_post_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+			<?php if ($inTheField_query->have_posts()) : ?>
+			 	<?php while ($inTheField_query->have_posts()) : $inTheField_query->the_post();  ?>
+
+					<?php $thisID = get_the_ID(); 
+					//logit( $thisID, '$thisID:' ); ?>
 					
-					<?php $thisID = get_the_ID(); ?>
-			 		
-
-					<?php if ( has_post_thumbnail() ) {
-
-    				//the_post_thumbnail();
-    				echo get_the_post_thumbnail($page->ID, 'thumbnail');
-
-    				} else {
-					//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
-					 //echo '<img src="';
-					 //echo catch_that_news_image();
-					// echo '" alt="" />';
-
-    					 
-					//get article image from flickr
-					grow_get_article_image($size='thumbnail');
-
-
-					} ?>
-
-					//todo
-					<h1 class="entry-title"><a href="<?php echo get_post_permalink($thisID); ?>"><?php the_title(); ?></a></h1>
-
-					<?php 
-					if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-						the_post_thumbnail();
-					} 
-					?>
-
-					<p><?php the_excerpt(); ?> </p>
+					
+					<?php get_template_part( 'content', 'inTheField' ); ?>
 
 			 	<?php endwhile; ?>
 			<?php endif; ?>	
